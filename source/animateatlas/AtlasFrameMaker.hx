5fabe06b9a6ba96472f2f488a78dba0bdcd35632
Ralsei <119281847+RalseiSmol03@users.noreply.github.com>
2023-06-09 05:48:02 +0800

    update source/animateatlas/AtlasFrameMaker.hx

--- source/animateatlas/AtlasFrameMaker.hx
+++ source/animateatlas/AtlasFrameMaker.hx
@@ -13,12 +13,12 @@ import animateatlas.displayobject.SpriteMovieClip;
 import flixel.graphics.FlxGraphic;
 import flixel.graphics.frames.FlxFramesCollection;
 import flixel.graphics.frames.FlxFrame;
-#if desktop
+#if desktop || android
 import sys.FileSystem;
 import sys.io.File;
 #else
-//import js.html.FileSystem;
-//import js.html.File;
+import js.html.FileSystem;
+import js.html.File;
 #end
 
 using StringTools;
