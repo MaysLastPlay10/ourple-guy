7c25fe3efad07714c1876eca6a6d687a13e2b01a
Ralsei <119281847+RalseiSmol03@users.noreply.github.com>
2023-06-19 07:45:06 +0800

    update source/Paths.hx

--- source/Paths.hx
+++ source/Paths.hx
@@ -153,7 +153,7 @@ class Paths
 
 	inline public static function getPreloadPath(file:String = '')
 	{
-		return SUtil.getStorageDirectory() + 'assets/$file';
+		return 'assets/$file';
 	}
 
 	inline static public function file(file:String, type:AssetType = TEXT, ?library:String)
