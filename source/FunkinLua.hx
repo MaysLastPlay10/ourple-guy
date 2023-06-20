2166e4e02c12f7ced0e965219071688c2af6bf84
Ralsei <119281847+RalseiSmol03@users.noreply.github.com>
2023-06-20 15:47:55 +0800

    .

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -44,8 +44,8 @@ import Discord;
 using StringTools;
 
 class FunkinLua {
-	public static var Function_Stop:Dynamic = 1;
-	public static var Function_Continue:Dynamic = 0;
+	public static var Function_Stop:Dynamic = "##PSYCHLUA_FUNCTIONSTOP";
+	public static var Function_Continue:Dynamic = "##PSYCHLUA_FUNCTIONCONTINUE";
 
 	#if LUA_ALLOWED
 	public var lua:State = null;
